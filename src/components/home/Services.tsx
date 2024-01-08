664448df49c862f57c4f64ea803ce27cc373140a
Clement SERIZAY <clement.serizay@gmail.com>
2024-01-08 16:11:40 +0100

    fix: shadcn bug

--- src/components/home/Services.tsx
+++ src/components/home/Services.tsx
@@ -1,16 +1,12 @@
-import React from 'react';
-import {
-	Carousel,
-	CarouselContent,
-	CarouselItem,
-	CarouselNext,
-	CarouselPrevious,
-} from '@/components/ui/carousel';
+'use client';
+
+
 
 const Services = () => {
 	return (
-		<div className='h-[50vh] bg-primary text-white  py-6'>
+		<div className='h-[50vh] bg-primary text-white flex flex-col gap-8 py-6'>
 			<h2 className='text-2xl font-[600] text-center'>Nos Services</h2>
+		
 		</div>
 	);
 };
