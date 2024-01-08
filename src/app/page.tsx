6c8d47391183637537e902fcb0a62e2891f3b724
Clement SERIZAY <clement.serizay@gmail.com>
2024-01-08 14:38:59 +0100

    feat: finish intro

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Introduction from '@/components/Introduction';
+import Objectives from '@/components/Objectives';
 
 export default async function Home() {
 	return (
 		<main>
 			<Introduction />
+			<Objectives />
 		</main>
 	);
 }
---
+++ src/components/Objectives.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Objectives = () => {
+  return <div className='h-[100vh] bg-cardio-irm text-white'>Objectives</div>;
+}
+
+export default Objectives
