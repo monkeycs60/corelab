bd23050d1b705ea7e7686e0ff0f9fcd589cf0582
Clement SERIZAY <clement.serizay@gmail.com>
2024-01-08 13:01:46 +0100

    fix: font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
 	children: React.ReactNode;
 }) {
 	return (
-		<html lang='en' className='!scroll-smooth relative font-generalRegular'>
+		<html lang='en' className='!scroll-smooth relative'>
 			<body className=''>
 				<Header />
 				{children}
