784c8e9656ce7092973dc154844dc36d0e56775a
Clement SERIZAY <clement.serizay@gmail.com>
2024-01-09 22:27:52 +0100

    style: responsive desktop stats

--- src/components/home/Services.tsx
+++ src/components/home/Services.tsx
@@ -5,10 +5,10 @@ import CarouselSlider from './CarouselSlider';
 const Services = () => {
 	return (
 		<div
-			className=' bg-primary text-white flex flex-col gap-8 pt-16 pb-24 md:gap-16'
+			className=' bg-primary text-white flex flex-col gap-8 pb-20 pt-6 md:pb-24 md:pt-16 md:gap-16'
 			id='services'>
 			<h2 className='text-2xl md:text-4xl font-[600] text-center'>Nos Services</h2>
-				<CarouselSlider />
+			<CarouselSlider />
 		</div>
 	);
 };
--- src/components/home/Statistics.tsx
+++ src/components/home/Statistics.tsx
@@ -18,27 +18,27 @@ const Statistics = () => {
 	return (
 		<div
 			ref={ref}
-			className='  text-primary flex flex-col gap-8 pt-8 pb-20 px-8'
+			className='  text-primary flex flex-col gap-8 pt-8 pb-16 px-8 md:gap-8 md:pt-16 md:pb-24'
 			id='statistics'>
-			<h2 className='text-2xl font-[600] text-center'>
+			<h2 className='text-2xl font-[600] text-center md:text-4xl'>
 				Quelques statistiques
 			</h2>
-			<p className='text-center'>
+			<p className='text-center md:text-lg'>
 				La base de données de <span className='font-semibold'>MIRACL</span>
 				<span className='font-semibold text-secondary'>.ai</span> est
 				constituée aujourd’hui de :
 			</p>
-			<div className='flex flex-col gap-6'>
-				<div className='flex items-center'>
+			<div className='flex flex-col gap-6 md:flex-row md:justify-between md:w-[60%] md:mt-10 md:m-auto md:gap-0'>
+				<div className='flex items-center md:flex-col md:justify-center'>
 					<Image
 						src='/stat1.svg'
-						className='h-20 w-20'
+						className='h-20 w-20 md:h-28 md:w-28'
 						width={100}
 						height={125}
 						alt='studies'
 					/>
-					<div className='flex-1 text-[#00A8B3] text-center'>
-						<span className='text-3xl font-bold'>
+					<div className='flex-1 text-[#00A8B3] text-center md:flex-auto md:w-full'>
+						<span className='text-3xl font-bold md:text-7xl'>
 							{
 								hasAnimated ? <CountUp end={12} duration={4} /> : 12
 							}
@@ -47,16 +47,16 @@ const Statistics = () => {
 					</div>
 				</div>
 
-				<div className='flex items-center'>
+				<div className='flex items-center  md:flex-col'>
 					<Image
 						src='/stat2.svg'
-						className='h-18 w-20'
+						className='h-18 w-20 md:h-[105px] md:w-[105px]'
 						width={110}
 						height={100}
 						alt='studies'
 					/>
-					<div className='w-3/4  text-[#FF6B00] text-center'>
-						<span className='text-3xl font-bold'>
+					<div className='w-3/4  text-[#FF6B00] text-center md:w-full'>
+						<span className='text-3xl font-bold md:text-7xl'>
 							{
 								hasAnimated ? <CountUp end={28256} duration={4} /> : 28256
 							}
@@ -65,16 +65,16 @@ const Statistics = () => {
 					</div>
 				</div>
 
-				<div className='flex items-center'>
+				<div className='flex items-center  md:flex-col md:items-center md:justify-center'>
 					<Image
 						src='/stat3.svg'
-						className='h-20 w-20'
+						className='h-20 w-20 md:h-[100px]  md:w-[100px] '
 						width={113}
 						height={117}
 						alt='studies'
 					/>
-					<div className='w-3/4 text-secondary text-center'>
-						<span className='text-3xl font-bold'>
+					<div className='w-3/4 text-secondary text-center md:w-full'>
+						<span className='text-3xl font-bold md:text-7xl'>
 							{
 								hasAnimated ? <CountUp end={8564026} duration={4} /> : 8564026
 							}
