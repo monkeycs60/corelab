372a0d965b315f9ac5e608c6b61c68396704261c
Clement SERIZAY <clement.serizay@gmail.com>
2024-01-09 14:53:10 +0100

    refactor: make subcomponent for team member

--- src/components/home/Team.tsx
+++ src/components/home/Team.tsx
@@ -1,66 +1,22 @@
 import Image from 'next/image';
-import React from 'react'
+import React from 'react';
+import TeamMember from './TeamMember';
 
 const Team = () => {
-  return (
+	return (
 		<div className=' bg-primary text-white flex flex-col gap-8 pt-8 pb-20'>
 			<h2 className='text-2xl font-[600] text-center'>
 				Les responsables de la plateforme
 			</h2>
 			<div className='flex flex-wrap items-center justify-center gap-4'>
-				<div className='w-[40%] flex flex-col items-center gap-2'>
-					<Image
-						src='/theo.jpg'
-						alt='Picture of the author'
-						width={720}
-						height={720}
-						className='h-28 w-28 rounded-full border-[2px] '
-					/>
-					<p>Dr Théo Pezel</p>
-				</div>
-				<div className='w-[40%] flex flex-col items-center gap-2'>
-					<Image
-						src='/theo.jpg'
-						alt='Picture of the author'
-						width={720}
-						height={720}
-						className='h-28 w-28 rounded-full border-[2px] '
-					/>
-					<p>Dr Théo Pezel</p>
-				</div>
-				<div className='w-[40%] flex flex-col items-center gap-2'>
-					<Image
-						src='/theo.jpg'
-						alt='Picture of the author'
-						width={720}
-						height={720}
-						className='h-28 w-28 rounded-full border-[2px] '
-					/>
-					<p>Dr Théo Pezel</p>
-				</div>
-				<div className='w-[40%] flex flex-col items-center gap-2'>
-					<Image
-						src='/theo.jpg'
-						alt='Picture of the author'
-						width={720}
-						height={720}
-						className='h-28 w-28 rounded-full border-[2px] '
-					/>
-					<p>Dr Théo Pezel</p>
-				</div>
-				<div className='w-[40%] flex flex-col items-center gap-2'>
-					<Image
-						src='/theo.jpg'
-						alt='Picture of the author'
-						width={720}
-						height={720}
-						className='h-28 w-28 rounded-full border-[2px] '
-					/>
-					<p>Dr Théo Pezel</p>
-				</div>
+				<TeamMember TeamNumber={1} descriptionContent='Dr Théo Pezel' />
+				<TeamMember TeamNumber={1} descriptionContent='Dr Théo Pezel' />
+				<TeamMember TeamNumber={1} descriptionContent='Dr Théo Pezel' />
+				<TeamMember TeamNumber={1} descriptionContent='Dr Théo Pezel' />
+				<TeamMember TeamNumber={1} descriptionContent='Dr Théo Pezel' />
 			</div>
 		</div>
-  );
-}
+	);
+};
 
-export default Team
+export default Team;
---
+++ src/components/home/TeamMember.tsx
@@ -0,0 +1,25 @@
+import Image from 'next/image';
+import React from 'react';
+
+type TeamMemberProps = {
+	TeamNumber: number;
+	descriptionContent: string;
+};
+
+const TeamMember = ({ TeamNumber, descriptionContent }: TeamMemberProps) => {
+	return (
+		<div className='w-[40%] flex flex-col items-center gap-2'>
+			<Image
+				src={`/team${TeamNumber}.jpg`}
+				alt={`team member number ${TeamNumber}`}
+				color='#F33349'
+				className='h-28 w-28 rounded-full border-[2px] '
+				width={720}
+				height={720}
+			/>
+			{descriptionContent}
+		</div>
+	);
+};
+
+export default TeamMember;
