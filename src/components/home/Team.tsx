79c664be4f18d8e294397502995bcc0fcd3be3a4
Clement SERIZAY <clement.serizay@gmail.com>
2024-01-10 15:20:01 +0100

    style: remaster wide screen

--- src/components/home/Team.tsx
+++ src/components/home/Team.tsx
@@ -3,26 +3,28 @@ import TeamMember from './TeamMember';
 const Team = () => {
 	return (
 		<div
-			className=' bg-primary text-white flex flex-col gap-10 pt-8 pb-12 md:pt-16 md:pb-24 md:gap-20'
+			className=' bg-primary text-white pt-8 pb-12 md:pt-16 md:pb-24'
 			id='team'>
-			<h2 className='text-2xl font-[600] text-center md:text-4xl md:text-left md:pl-20'>
-				Les responsables de la plateforme
-			</h2>
-			<div className='flex flex-wrap items-center justify-center md:justify-around gap-4 md:w-[90%] md:m-auto md:flex-nowrap'>
-				<TeamMember TeamNumber={1} descriptionContent='Dr Théo Pezel' />
-				<TeamMember TeamNumber={1} descriptionContent='Pr Patrick Henry' />
-				<TeamMember
-					TeamNumber={1}
-					descriptionContent='Dr Jean-Guillaume Dillinger'
-				/>
-				<TeamMember
-					TeamNumber={1}
-					descriptionContent='Dr Trecy Gonçalves'
-				/>
-				<TeamMember
-					TeamNumber={1}
-					descriptionContent='Dr Paul-Jun Martial'
-				/>
+			<div className='flex flex-col gap-10 md:gap-20 3xl:max-w-[1920px] 3xl:m-auto'>
+				<h2 className='text-2xl font-[600] text-center md:text-4xl md:text-left md:pl-20'>
+					Les responsables de la plateforme
+				</h2>
+				<div className='flex flex-wrap items-center justify-center md:justify-around gap-4 md:w-[90%] md:m-auto md:flex-nowrap'>
+					<TeamMember TeamNumber={1} descriptionContent='Dr Théo Pezel' />
+					<TeamMember TeamNumber={1} descriptionContent='Pr Patrick Henry' />
+					<TeamMember
+						TeamNumber={1}
+						descriptionContent='Dr Jean-Guillaume Dillinger'
+					/>
+					<TeamMember
+						TeamNumber={1}
+						descriptionContent='Dr Trecy Gonçalves'
+					/>
+					<TeamMember
+						TeamNumber={1}
+						descriptionContent='Dr Paul-Jun Martial'
+					/>
+				</div>
 			</div>
 		</div>
 	);
