38ea3ed930f34c1f05df0d23f5191b907e28db75
Clement SERIZAY <clement.serizay@gmail.com>
2024-01-10 12:24:29 +0100

    style: wide screen edit

--- src/components/home/Introduction.tsx
+++ src/components/home/Introduction.tsx
@@ -66,9 +66,9 @@ const Introduction = () => {
 			<div
 				className='pt-[5rem] h-screen text-white bg-cardio-computer-desktop justify-end hidden md:flex'
 				id='introduction'>
-				<div className='px-24 3xl:px-48 text-center md:w-[50%] gap-12  mt-20 flex flex-col 3xl:w-[38%] 3xl:mt-36 3xl:gap-24 '>
+				<div className='3xl:px-48 text-center md:w-[40%] gap-12  mt-20 flex flex-col items-center 3xl:w-[38%] 3xl:mt-36 3xl:gap-24'>
 					<h2 className='text-3xl font-[600]'>Qu’est-ce que MIRACL<span className="text-secondary">.ai</span> ?</h2>
-					<div className='flex flex-col gap-8 font-thin tracking-wide text-md 3xl:text-xl 3xl:leading-9 3xl:gap-12'>
+					<div className='flex flex-col gap-8 font-thin tracking-wide text-md w-[400px]  3xl:w-[600px] 3xl:text-xl 3xl:leading-9 3xl:gap-12'>
 						<TextIntro />
 					</div>
 				</div>
