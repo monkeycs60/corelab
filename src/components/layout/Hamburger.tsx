72732ddc8e990ad26f4ff027b348354702f93ad0
Clement SERIZAY <clement.serizay@gmail.com>
2024-01-11 11:11:16 +0100

    feat: add hamburger menu

---
+++ src/components/layout/Hamburger.tsx
@@ -0,0 +1,85 @@
+'use client';
+
+import { motion } from 'framer-motion';
+import { X } from 'lucide-react';
+import Link from 'next/link';
+
+interface HamburgerProps {
+    isHamburgerOpen: boolean;
+    toggleHamburgerMenu: () => void;
+}
+const Hamburger = ({
+    isHamburgerOpen,
+    toggleHamburgerMenu,
+}: HamburgerProps) => {
+
+    const variants = {
+        open: { opacity: 1, y: 0, height: "75%" },
+        closed: { opacity: 0, y: '-100%', height: "0%" },
+        exit: { opacity: 0, y: '-100%', height: "0%" },
+    };
+    return (
+        <motion.section
+            initial='closed'
+            animate={isHamburgerOpen ? 'open' : 'closed'}
+            exit='exit'
+            variants={variants}
+            transition={{ duration: 0.5 }}
+            className='fixed left-0 top-0 z-[100] h-screen w-screen overflow-hidden bg-primary text-white font-inter'
+        >
+            <div className=' flex h-full flex-col  justify-center p-12 text-center text-lg mt-6 sm:text-xl '>
+                <div className='flex flex-col gap-6'>
+                    <Link
+                        href={'#introduction'}
+                        className='linkHoverEffect'
+                        onClick={toggleHamburgerMenu}
+                    >
+                        Présentation
+                    </Link>
+                    <Link
+                        href={'#objectives'}
+                        className='linkHoverEffect'
+                        onClick={toggleHamburgerMenu}
+                    >
+                        Objectifs
+                    </Link>
+                    <Link
+                        href={'#services'}
+                        className='linkHoverEffect'
+                        onClick={toggleHamburgerMenu}
+                    >
+                        Services
+                    </Link>
+                    <Link
+                        href={'/statistics'}
+                        className='linkHoverEffect'
+                        onClick={toggleHamburgerMenu}
+                    >
+                        Statistiques
+                    </Link>
+                    <Link
+                        href={'/statistics'}
+                        className='linkHoverEffect'
+                        onClick={toggleHamburgerMenu}
+                    >
+                        Responsables
+                    </Link>
+                    <Link
+                        href={'/statistics'}
+                        className='linkHoverEffect underline underline-offset-1'
+                        onClick={toggleHamburgerMenu}
+                    >
+                        Contacts
+                    </Link>
+                </div>
+            </div>
+            <X
+                size={40}
+                className='absolute right-3 top-4'
+                onClick={toggleHamburgerMenu}
+            />
+        </motion.section>
+    );
+};
+
+export default Hamburger;
--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,58 +1,76 @@
 "use client";
 
 import Image from 'next/image';
-import { Menu } from 'lucide-react';
+import { Menu, X } from 'lucide-react';
 import Link from 'next/link';
 import { useWindowScroll } from "@uidotdev/usehooks";
+import { AnimatePresence } from 'framer-motion';
+import useHamburgerMenu from '@/hooks/useHamburgerMenu';
+import Hamburger from './Hamburger';
 
 const Header = () => {
+	const { isHamburgerOpen, toggleHamburgerMenu } = useHamburgerMenu();
 	const [{ y }] = useWindowScroll();
 
 	return (
-
-		<header className={`z-[999] fixed top-0 w-full h-[5.5rem] bg-[#070F1E] text-white  py-2 px-5 transition-all duration-200 md:px-16 md:h-[6rem] 3xl:h-[7rem]    ${y && y > 50 ? 'md:bg-[#070F1E]' : 'md:bg-transparent'
-			} `}>
-			<div className='m-auto flex justify-between items-center h-full 3xl:max-w-[1920px] 3xl:h-full'>
-				<Link href="#introduction" className='flex items-center gap-4 md:gap-8'>
-					<Image
-						src='/miracl-logo.svg'
-						className='w-14 md:w-[70px]'
-						width={452}
-						height={402}
-						alt='Corelab logo'
+		<>
+			<AnimatePresence mode='wait'>
+				{isHamburgerOpen && (
+					<Hamburger
+						isHamburgerOpen={isHamburgerOpen}
+						toggleHamburgerMenu={toggleHamburgerMenu}
 					/>
-					<h1>
-						<span className='font-semibold text-2xl md:text-3xl'>MIRACL</span>
-						<span className='text-secondary text-xl md:text-2xl'>.ai</span>
-					</h1>
-				</Link>
-				<nav className='md:w-[500px]'>
-					<Menu className='w-8 h-8 md:hidden' />
-					<ul className='gap-8 justify-between hidden md:flex'>
-						<Link
-							href='#objectives'
-							className='hover:text-secondary transition-all duration-200'>
-							<li>Objectifs</li>
-						</Link>
-						<Link
-							href='#services'
-							className='hover:text-secondary transition-all duration-200'>
-							<li>Services</li>
-						</Link>
-						<Link
-							href='#team'
-							className='hover:text-secondary transition-all duration-200'>
-							<li>Equipe</li>
-						</Link>
-					</ul>
-				</nav>
-				<Link href='#contact' className='hidden md:block'>
-					<button className='bg-secondary text-white font-semibold px-8 py-3 rounded-md hover:bg-white hover:text-secondary transition-all duration-300 3xl:px-10 3xl:py-4'>
-						Contact
-					</button>
-				</Link>
-			</div>
-		</header>
+				)}
+			</AnimatePresence>
+
+			<header className={`z-[999] fixed top-0 w-full h-[5.5rem] bg-[#070F1E] text-white  py-2 px-5 transition-all duration-200 md:px-16 md:h-[6rem] 3xl:h-[7rem]    ${y && y > 50 ? 'md:bg-[#070F1E]' : 'md:bg-transparent'
+				} `}>
+				<div className='m-auto flex justify-between items-center h-full 3xl:max-w-[1920px] 3xl:h-full'>
+					<Link href="#introduction" className='flex items-center gap-4 md:gap-8'>
+						<Image
+							src='/miracl-logo.svg'
+							className='w-14 md:w-[70px]'
+							width={452}
+							height={402}
+							alt='Corelab logo'
+						/>
+						<h1>
+							<span className='font-semibold text-2xl md:text-3xl'>MIRACL</span>
+							<span className='text-secondary text-xl md:text-2xl'>.ai</span>
+						</h1>
+					</Link>
+					<nav className='md:w-[500px]'>
+						<div className='w-8 h-8 md:hidden'>
+							{
+								isHamburgerOpen ? <X onClick={toggleHamburgerMenu} className='h-full w-full' /> : <Menu onClick={toggleHamburgerMenu} className='h-full w-full' />
+							}
+						</div>
+						<ul className='gap-8 justify-between hidden md:flex'>
+							<Link
+								href='#objectives'
+								className='hover:text-secondary transition-all duration-200'>
+								<li>Objectifs</li>
+							</Link>
+							<Link
+								href='#services'
+								className='hover:text-secondary transition-all duration-200'>
+								<li>Services</li>
+							</Link>
+							<Link
+								href='#team'
+								className='hover:text-secondary transition-all duration-200'>
+								<li>Equipe</li>
+							</Link>
+						</ul>
+					</nav>
+					<Link href='#contacts' className='hidden md:block'>
+						<button className='bg-secondary text-white font-semibold px-8 py-3 rounded-md hover:bg-white hover:text-secondary transition-all duration-300 3xl:px-10 3xl:py-4'>
+							Contact
+						</button>
+					</Link>
+				</div>
+			</header>
+		</>
 	);
 };
 
