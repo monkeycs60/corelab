a7edcac04ef68442e174194252825b5769adfd7b
Clement SERIZAY <clement.serizay@gmail.com>
2024-01-11 10:26:37 +0100

    style: change padding

--- src/components/home/ServiceCard.tsx
+++ src/components/home/ServiceCard.tsx
@@ -9,7 +9,7 @@ type ServiceCardProps = {
 
 const ServiceCard = ({ logoSrc, title, description }: ServiceCardProps) => {
 	return (
-		<div className='flex flex-col gap-2 justify-between text-center text-sm items-center bg-white text-black rounded-xl w-full h-[300px] py-6 px-4 md:h-auto md:gap-1 md:items-start md:w-[30%] md:text-lg md:px-4 3xl:px-8 3xl:w-[27%] 3xl:h-[300px] 3xl:py-8'>
+		<div className='flex flex-col gap-2 justify-between text-center text-sm items-center bg-white text-black rounded-xl w-full h-[300px] py-6 px-4 md:h-auto md:gap-1 md:items-start md:w-[30%] md:text-lg md:py-4 md:px-4 3xl:px-8 3xl:w-[27%] 3xl:h-[300px] 3xl:py-8'>
 			<div className='h-16 md:h-12 md:flex md:items-start 3xl:h-16'>
 				<Image priority src={logoSrc} alt='logo' width={48} height={48} className='' />
 			</div>
