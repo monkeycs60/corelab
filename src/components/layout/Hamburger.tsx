2397a9790a2bcd56ddf08719c241f08d4296a0ec
monkeycs60 <clement.serizay@gmail.com>
2024-01-12 16:34:15 +0100

    style: change height

--- src/components/layout/Hamburger.tsx
+++ src/components/layout/Hamburger.tsx
@@ -14,7 +14,7 @@ const Hamburger = ({
 }: HamburgerProps) => {
 
     const variants = {
-        open: { opacity: 1, y: 0, height: "70%" },
+        open: { opacity: 1, y: 0, height: "75%" },
         closed: { opacity: 0, y: '-100%', height: "0%" },
         exit: { opacity: 0, y: '-100%', height: "0%" },
     };
