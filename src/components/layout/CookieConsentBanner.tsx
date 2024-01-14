a76748e7c9fc03b4006870a8ff672294d49db169
monkeycs60 <clement.serizay@gmail.com>
2024-01-14 17:15:23 +0100

    style: change cookie banner pos on mobile

--- src/components/layout/CookieConsentBanner.tsx
+++ src/components/layout/CookieConsentBanner.tsx
@@ -30,7 +30,7 @@ const CookieConsentBanner = () => {
     }
 
     return (
-        <div className="fixed flex-col text-center md:text-left md:flex-row text-xs md:text-sm bottom-2 md:bottom-0 left-0 z-[999999] bg-primary/80 text-white w-full flex gap-4 justify-between px-8 py-4 items-center ">
+        <div className="fixed flex-col text-center md:text-left md:flex-row text-xs md:text-sm bottom-0 left-0 z-[999999] bg-primary/80 text-white w-full flex gap-4 justify-between px-8 py-4 items-center ">
             <p>Ce site utilise des cookies pour améliorer votre expérience de navigation.</p>
             <div className="flex gap-8">
             <button onClick={handleAccept} className="border-[1px] p-2 bg-white text-primary rounded-lg hover:bg-primary hover:text-white">Accepter les cookies</button>
